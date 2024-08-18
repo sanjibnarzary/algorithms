4aab70852a01d742a3629a0d089b2226c0769ce0
Sanjib Narzary <o-._.-o@live.com>
2024-08-18 08:52:19 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,35 @@
 /* eslint-disable */
 const { i18n } = require("./next-i18next.config");
 
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/algorithms",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
+
+//export default nextConfig;
 module.exports = {
-  i18n,
+  i18n, nextConfig,
 };
